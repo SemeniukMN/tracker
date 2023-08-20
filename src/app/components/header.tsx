8c4312572014e8ced1540e4b141a0bfb86cb890d
Maxim Semeniuk <msemenuk22@gmail.com>
2023-08-20 20:24:06 +0500

    Add prisma ORM

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-export default function Header({className}) {
+export default function Header({className}: {className: string}) {
   return (
     <header className={`h-12 p-4 shadow bg-white rounded-lg flex items-center ${className}`}>
-      <Link href="/">LOGO</Link>
+      <Link href="/" className="text-xl">Tracker</Link>
       <nav className="mx-auto flex gap-5">
         <Link href="../dashboard">Home</Link>
         <Link href="../dashboard">Dashboard</Link>
